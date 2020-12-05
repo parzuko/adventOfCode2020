6bc08074e8f6e7cd8d26aa1379e102398774c1d4
parzuko <jivansh17@gmail.com>
2020-12-05 12:17:01 +0530

    add second solution to day3

--- day3/trajectory.py
+++ day3/trajectory.py
@@ -25,10 +25,24 @@ def traverse_map(map, right_increment, down_increment):
 
     return count
 
+# Solution to part two
+def different_paths_on_same_slope(tree_map):
+    tree_count = traverse_map(map=tree_map, right_increment=1, down_increment=1)
+    tree_count *= traverse_map(map=tree_map, right_increment=3, down_increment=1)
+    tree_count *= traverse_map(map=tree_map, right_increment=5, down_increment=1)
+    tree_count *= traverse_map(map=tree_map, right_increment=7, down_increment=1)
+    tree_count *= traverse_map(map=tree_map, right_increment=1, down_increment=2)
+
+    return tree_count
+
+
+
 def main():
     tree_map = parse_input()
     tree_count = traverse_map(map=tree_map, right_increment=3, down_increment=1)
     print(f"There are {tree_count} trees in the way!")
+    second_solution = different_paths_on_same_slope(tree_map)
+    print(f"The product of all the trees in the path is : {second_solution}")
 
 if __name__ == "__main__":
     main()
